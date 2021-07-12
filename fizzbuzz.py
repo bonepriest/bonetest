bfc674ba9f86a8f4fe7b00240c1eebb433716659
bonepriest <simpleimmortal713@gmail.com>
2021-07-13 00:02:49 +0400

    "[FIZZBUZZ][MODIFIED]fizzbuzz.py:added the functions to convert from roman to decimal and vice versa"

---
+++ fizzbuzz.py
@@ -0,0 +1,29 @@
+def to_roman(dinput):
+    romanlist = [(1000,"M"), (900,"CM") (500,"D"), (400,"CD"), (100,"C"), (90,"XC"), (50,"L"), (40, "XL"), (10,"X"), (9,"IX"), (5,"V"), (4,"IV"), (1, "I")]
+    roman_threshold, roman_numeral = romanlist
+    for roman_threshold, roman_numeral in romanlist:
+       if dinput > roman_threshold:
+           print(roman_numeral)
+           dinput -= roman_threshold
+    return dinput
+
+
+def to_decimal(rinput):
+    doutput = 0
+    decimal_list = [(1000,"M"), (900,"CM") (500,"D"), (400,"CD"), (100,"C"), (90,"XC"), (50,"L"), (40, "XL"), (10,"X"), (9,"IX"), (5,"V"), (4,"IV"), (1, "I")]
+    decimal_number, roman_digits = decimal_list
+    rinput_digits = [char for char in rinput]
+    for char in rinput_digits:
+        for roman_digits in decimal_list:
+            if char == roman_digits:
+                doutput += decimal_number
+    return doutput
+
+
+x = input("Enter 1 to convert a decimal number to roman form, 2 to convert a roman number to decimal form.")
+if x == 1:
+    decimal_input = input("Enter decimal number.")
+    to_roman(decimal_input)
+elif x == 2:
+    roman_input =  input("Enter roman number.")
+    to_decimal(roman_input)
