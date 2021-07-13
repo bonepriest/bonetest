40c652a09d51c2b252b14f6cfc522e28485130e6
bonepriest <simpleimmortal713@gmail.com>
2021-07-13 22:32:39 +0400

    [FIZZBUZZ][BUGFIX]fizzbuzz.py:fixed the logic error in the to_roman function, changed the function so that result is returned to call instead of printed from inside the function.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,12 @@
 def to_roman(dinput):
     dinput = int(dinput)
+    doutput = ''
     romanlist = [(1000,"M"),(900,"CM"), (500,"D"), (400,"CD"), (100,"C"), (90,"XC"), (50,"L"), (40, "XL"), (10,"X"), (9,"IX"), (5,"V"), (4,"IV"), (1, "I")]
     for roman_threshold, roman_numeral in romanlist:
-       if dinput > roman_threshold:
-           print(roman_numeral)
+       while dinput >= roman_threshold:
+           doutput = doutput + roman_numeral
            dinput -= roman_threshold
-    return dinput
+    return doutput
 
 
 def to_decimal(rinput):
@@ -22,7 +23,7 @@ def to_decimal(rinput):
 x = input("Enter 1 to convert a decimal number to roman form, 2 to convert a roman number to decimal form.")
 if x == "1":
     decimal_input = input("Enter decimal number.")
-    to_roman(decimal_input)
+    print(to_roman(decimal_input))
 elif x == "2":
     roman_input = input("Enter roman number.")
     print(to_decimal(roman_input))
